00b70b325d422f3f84e90e25605bad171669909c
MechnoLowPing <marius.will@stud.thga.de>
2022-01-29 18:12:05 +0100

    mainloop.hpp wurde eingefügt

---
+++ src/mainloop.hpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"calculate.hpp"
+
+namespace mainloop{
+    void mainfunction()
+        {
+            for(;;)
+                {
+                    int option;
+
+                    std::cout<<"Geben Sie eine Zahl zwischen 0 und 4 ein!"<<std::endl;
+                    std::cin>>option;
+
+                    switch(option){
+
+                        default: std::cout<<"Fehler!!! Zahl zwischen 0 und 1 benötigt!"<<std::endl; break;
+                        case 1: calculate::addition(); break;
+                        case 0: return;
+
+                    }
+                }
+
+        }
+}
